845eedf170fc8d57768646825bc7e594b5c71501
Galen Guyer <galen@galenguyer.com>
2020-11-11 16:24:05 -0500

    get ip when run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use std::io::Read;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut resp = reqwest::blocking::get("https://ip.nyaa.gay/ip")?;
+
+    let mut body = String::new();
+    resp.read_to_string(&mut body)?;
+
+    println!("{}", body.trim());
+    Ok(())
 }
